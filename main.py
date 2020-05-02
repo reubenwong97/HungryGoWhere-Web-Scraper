25d81012460f4e8aa28f1775dbf62d6ea442a22a
reubenwong97 <reuben.wong.ys@gmail.com>
2020-05-02 20:36:27 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,15 +8,16 @@ from gov_parser import get_hawker_names
 from collections import defaultdict
 import pandas as pd
 
-cheat_list = ['Tanglin Halt Market', 'Tekka Market', 'Tiong Bahru Market', 'Zion Riverside Food Centre']
-# 'Taman Jurong Market & Food Centre', 
+# Cheat list exists for names stored differently in HungryGoWhere and data.gov.sg's database
+# cheat_list = ['Tanglin Halt Market', 'Tekka Market', 'Tiong Bahru Market', 'Zion Riverside Food Centre']
+
 driver = webdriver.Chrome('C:/Users/Reuben/Desktop/chromedriver_win32/chromedriver')
 data = defaultdict(list)
 base_url = 'https://www.hungrygowhere.com/building/'
-# gov_api_url = 'https://data.gov.sg/api/action/datastore_search?resource_id=8f6bba57-19fc-4f36-8dcf-c0bda382364d&limit=107'
-# hawker_names = get_hawker_names(gov_api_url)
+gov_api_url = 'https://data.gov.sg/api/action/datastore_search?resource_id=8f6bba57-19fc-4f36-8dcf-c0bda382364d&limit=107'
+hawker_names = get_hawker_names(gov_api_url)
 
-for hawker_centre_name in cheat_list:
+for hawker_centre_name in hawker_names:
     query_name = query_gen(hawker_centre_name)
     query_url = base_url + query_name + '/'
 
@@ -38,5 +39,5 @@ for hawker_centre_name in cheat_list:
         print(key, len(data[key]))
 
     hawkerDf = pd.DataFrame.from_dict(data) 
-    hawkerDf.to_csv('hawker_cheat.csv', index=False)
+    hawkerDf.to_csv('hawker_final.csv', index=False)
 
